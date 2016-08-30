a02f5f89f5c012060c460053e9de8400e052b882
Erica <ericastephan@gmail.com>
2016-08-29 20:11:02 -0700

    solved through problem 2

--- src/transformYourLife.js
+++ src/transformYourLife.js
@@ -1,26 +1,42 @@
 var loop = function(collection, callback){
 
-  if (Array.isArray(collection)){
+  if (Array.isArray(collection) || typeof collection === 'string'){
     for (var i = 0; i < collection.length; i++){
       callback(collection[i], i);
     }
-  } else if (typeof collection === 'object'){
+	} 
+{
+  else if (typeof collection === 'object'){
     for (var key in collection){
       callback(collection[key], key);
     }
+
   }
 };
 
 
 // 1. Build transform.
-
+var transform = function(collection, callback){
+	var results = [];
+	loop(collection, function(item){
+		results.push(callback(item));
+	});
+	return results;
+};
 
 // 2. allNumbersMultipliedByThree
 var numbers = [1, 2, 3, 4, 5];
 
+var allNumbersMultipliedByThree = transform(numbers,function(item){
+	return item*3;
+	});
+
+console.log(allNumbersMultipliedByThree);
 
 // 3. bestSentenceToUpperCase
 var bestSentence = "This is the best six week course ever!";
+
+var bestSentenceToUpperCase = transform(bestSentence, function())
 
 // 4. collectedContents
 var person = {name: 'Jon', greatestFear: 'fearItself'};
